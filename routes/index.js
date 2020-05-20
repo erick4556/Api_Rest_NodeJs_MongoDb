c5cda1e788bcda0e274a34c6a4acb509ac514c2d
hideveloper20 <electronicmade9064@outlook.com>
2020-05-19 21:11:27 -0500

    Fixes para autorizacion

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,21 @@ var api = express.Router()
 //Llamadas de las funciones del controlador
 //api.get('/api/product',productController.getProducts) Quito la palabra api del principio por que en otro archivo digo que lo voy usar
 
-api.get('/product',productController.getProducts)
+api.get('/product', auth, productController.getProducts)
 
-api.get('/product/:productId',productController.getProduct)
+api.get('/product/:productId', auth, productController.getProduct)
 
-api.post('/product/post',productController.saveProduct);
+api.post('/product/post', auth, productController.saveProduct);
 
-api.put('/product/actualizar/:productId',auth,productController.updateProduct);
+api.put('/product/actualizar/:productId', auth, productController.updateProduct);
 
-api.delete('/product/eliminar/:productId',auth,productController.deleteProduct);
+api.delete('/product/eliminar/:productId', auth, productController.deleteProduct);
 
-api.post('/signup',userController.signUp)
-api.post('/signin',userController.signIn)
+api.post('/signup', userController.signUp)
+api.post('/signin', userController.signIn)
 
-api.get('/private',auth, (req,res)=>{
-	res.status(200).send({message:"Tienes acceso"})
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({ message: "Tienes acceso" })
 })
 
 module.exports = api
--- services/index.js
+++ services/index.js
@@ -1,52 +1,52 @@
-
 var jwt = require('jwt-simple')
 var moment = require('moment')
 var config = require('../config')
 
-function createToken(user){
-	var payload ={
-		sub:user._id, //id del usuario, pero no debería ser el de la base de datos
-		iat:moment().unix(), //Cuando fue creado el token. Devuelve tiempo en formato en unix
-		exp:moment().add(14,'days').unix() //Cuando va expirar el token
-	} //Datos que viajan del cliente al servidor
+function createToken(user) {
+    var payload = {
+            sub: user._id, //id del usuario, pero no debería ser el de la base de datos
+            iat: moment().unix(), //Cuando fue creado el token. Devuelve tiempo en formato en unix
+            exp: moment().add(14, 'days').unix() //Cuando va expirar el token
+        } //Datos que viajan del cliente al servidor
 
-	return jwt.encode(payload,config.SECRET_TOKEN) //Devuelve el token codificado
+    return jwt.encode(payload, config.SECRET_TOKEN) //Devuelve el token codificado
 
 }
 
-function decodeToken(token){
-	//promesa. resolve: Cuando la promesa esta resuelta, se ha resuelto la función que hay que llamar
-	//reject: Ha ocurrido un error y la promesa no se ha podido cumplir
-	var decoded = new Promise((resolve,reject)=>{
-		try{
+function decodeToken(token) {
+    //promesa. resolve: Cuando la promesa esta resuelta, se ha resuelto la función que hay que llamar
+    //reject: Ha ocurrido un error y la promesa no se ha podido cumplir
+    var decoded = new Promise((resolve, reject) => {
+        try {
 
-			var payload = jwt.decode(token,config.SECRET_TOKEN)
+            var payload = jwt.decode(token, config.SECRET_TOKEN)
 
-			if(payload.exp <= moment().unix()){
-				reject({
-					status:401,
-					message:'El token ha expirado'
-				})
-			}/*else{
-				resolve(payload.sub)
-			}*/
+            if (payload.exp <= moment().unix()) {
+                reject({
+                    status: 401,
+                    message: 'El token ha expirado'
+                })
+            }
+            /*else{
+            				resolve(payload.sub)
+            			}*/
 
-			resolve(payload.sub)
+            resolve(payload.sub)
 
-		}catch(err){
-			reject({
-				status:500,
-				message: 'Invalid token'
-			})
-		}
-	})
+        } catch (err) {
+            reject({
+                status: 500,
+                message: 'Invalid token'
+            })
+        }
+    })
 
-	return decoded
+    return decoded
 
 }
 
 
 module.exports = {
-	createToken,
-	decodeToken
+    createToken,
+    decodeToken
 }
